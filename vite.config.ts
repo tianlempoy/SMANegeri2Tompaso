3046537797073d0dcf27e5f81ed095c013f481d6
tianlempoy <christianlempoy011@gmail.com>
2026-04-20 18:35:27 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+});
