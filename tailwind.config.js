3c8c8974006995b99800306f9464bab332c9ac8d
tianlempoy <christianlempoy011@gmail.com>
2026-04-22 17:05:22 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
   ],
   theme: {
     extend: {
